6fe49d01aa6a789e835fdfd4cd79803a1f1fe500
GodsplanElot <elotgodsplan2@gmail.com>
2026-05-03 13:03:01 +0100

    feat: enhance PageWrapper layout with improved content structure and mobile navigation

--- cashflow_frontend/src/components/layout/PageWrapper.tsx
+++ cashflow_frontend/src/components/layout/PageWrapper.tsx
@@ -1,6 +1,7 @@
 // src/components/layout/PageWrapper.tsx
 
 import Sidebar from './Sidebar'
+import BottomNav from './BottomNav'
 
 interface PageWrapperProps {
   children: React.ReactNode
@@ -9,10 +10,16 @@ interface PageWrapperProps {
 export default function PageWrapper({ children }: PageWrapperProps) {
   return (
     <div className="flex h-screen bg-stone-50">
+      {/* Desktop Sidebar */}
       <Sidebar />
-      <main className="flex-1 overflow-auto">
+
+      {/* Main content */}
+      <main className="flex-1 overflow-auto pb-20 md:pb-0">
         {children}
       </main>
+
+      {/* Mobile Bottom Nav */}
+      <BottomNav />
     </div>
   )
 }
